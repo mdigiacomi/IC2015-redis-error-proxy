d2a3fb7d5b6df8643b38758823ee99e96c69c17d
Michael Di Giacomi <digitaladrenalin@gmail.com>
2015-11-07 19:37:09 -0500

    Added Facade Message Format

--- routes/RedisFacade.js
+++ routes/RedisFacade.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var redis = require("redis"),
-    client = redis.createClient('6379','elk.basenode.xyz','');
+    client = redis.createClient('6379','elk.digitaladrenalin.net','');
 
-/* GET home page. */
+var model = {
+    "Execution": "test",
+    "AppName": "IC Test Application",
+    "AppVersion": "v1.0.1",
+    "MessageType": "Info",
+    "isBusinessEvent": "true",
+    "Message": "This is a test Message",
+    "MessageDetails": "StackTrace Coming Soon...",
+    "AdditionalProperties": {
+        "Error Number": "1234"
+    }
+};
+
+
+    /* GET home page. */
 router.get('/', function(req, res, next) {
 
     var multi;
 
     multi = client.multi();
-    multi.rpush('logstash', arr[i]);
+    multi.rpush('logstash', model);
     multi.exec();
 
     res.send(200);
