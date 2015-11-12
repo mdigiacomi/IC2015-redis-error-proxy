4a4ce7000689cdc6fd5866c4362dde06f6415751
Michael Di Giacomi <basenode@vps58660.vps.ovh.ca>
2015-11-12 17:20:35 -0500

    Added Testing Method

--- routes/RedisFacade.js
+++ routes/RedisFacade.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var redis = require("redis"),
-    client = redis.createClient('6379','elk.digitaladrenalin.net','');
+    client = redis.createClient('6379','159.203.23.150','');
 
 var model = {
     "Execution": "test",
@@ -27,6 +27,30 @@ router.get('/', function(req, res, next) {
     multi.exec();
 
     res.send(200);
+});
+
+router.get('/error', function(req,res,next){
+	var multi;
+
+	model = {
+		"Execution": "test",
+   		"AppName": "IC Test Application",
+   		"AppVersion": "v1.0.1",
+    		"MessageType": "Error",
+    		"isBusinessEvent": "true",
+    		"Message": "This is a test Message",
+    		"MessageDetails": "StackTrace Coming Soon...",
+    		"AdditionalProperties": {
+        		"Error Number": "1234"
+    		}
+	};
+
+
+	multi = client.multi();
+	multi.rpush('logstash', model);
+	multi.exec();
+
+	res.send(200);
 });
 
 module.exports = router;
